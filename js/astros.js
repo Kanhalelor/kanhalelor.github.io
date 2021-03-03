0ed2af36c34de6f2dd23f575cac5737ad7b6d201
robSyntax <48064476+Kanhalelor@users.noreply.github.com>
2021-03-03 17:08:01 +0200

    initial commit

---
+++ js/astros.js
@@ -0,0 +1,47 @@
+// fetch api example
+
+const url = "http://api.open-notify.org/astros.json";
+const btn = document.getElementById("getastros");
+
+// what we want from data
+const astros = [];
+const crafts = [];
+
+// using fetch API
+const fetchData = () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => handleData(data))
+    .catch((err) => console.log(err));
+};
+
+const handleData = (data) => {
+  for (let i = 0; i < data.people.length; i++) {
+    astros.push(data.people[i].name);
+    crafts.push(data.people[i].craft);
+  }
+  drawHTML(astros, crafts);
+};
+// Draw astronauts
+const drawHTML = (astros, craft) => {
+  // Get images of astronauts
+  const urls = [
+    "https://www.nasa.gov/sites/default/files/thumbnails/image/jsc2020e014991.jpg",
+    "https://www.nasa.gov/sites/default/files/thumbnails/image/jsc2020e014993.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/c/c1/Ivan_Vagner_%28Jsc2020e014992%29.jpg"
+  ];
+  // make a card
+  let template = ``;
+  for (let i = 0; i < astros.length; i++) {
+    template += `
+   <div class="astro">
+      <h3 id="info">${astros[i]}: <span class="craft">${craft[i]}</span></h3>
+      <img src="${urls[i]}" alt="astro">
+    </div>
+   `;
+  }
+  document.querySelector(".astros").innerHTML = template;
+};
+
+// events
+btn.addEventListener("click", fetchData);
